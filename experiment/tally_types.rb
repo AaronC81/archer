c125808abe1f5d22612ce29cfbdb6601f004672c
Aaron Christiansen <aaronc20000@gmail.com>
2024-11-03 18:06:21 +0000

    Initial commit

---
+++ experiment/tally_types.rb
@@ -0,0 +1,23 @@
+# Tally operand types across all instructions
+
+require 'json'
+require_relative 'llvm_utils'
+
+instructions = load_instructions('J2')
+
+$types = {}
+def add_to_count(tys)
+  tys.each do |ty|
+    $types[ty] ||= 0
+    $types[ty] += 1
+  end
+end
+
+instructions.each do |_, ins|
+  next unless is_instruction?(ins)
+
+  add_to_count unwrap_flat_dag(ins['InOperandList']).map(&:first)
+  add_to_count unwrap_flat_dag(ins['OutOperandList']).map(&:first)
+end
+
+pp $types.sort_by { |_, count| count }.reverse
