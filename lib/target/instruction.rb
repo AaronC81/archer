fd5f7fc83246c3c778b29a6aa048b99d4f5c2e71
Aaron Christiansen <aaronc20000@gmail.com>
2024-11-14 21:39:47 +0000

    Apply operand constraints to improve readability
    See `or.ge.u` on TriCore for an example of an instruction affected by this

--- lib/target/instruction.rb
+++ lib/target/instruction.rb
@@ -21,6 +21,8 @@ class Target
       @implicitly_written_registers = defi.fetch('Defs').map { |reg| target.fetch_register(reg) }
 
       @pseudo = defi.fetch_bool('isPseudo')
+
+      apply_constraints(defi.fetch('Constraints'))
     end
 
     # @return [Symbol] LLVM's internal name for this instruction.
@@ -73,6 +75,69 @@ class Target
       fixup.modify.each do |var, value|
         instance_variable_set(var, value)
       end
+    end
+
+    # Fixes up an instruction, during loading from TableGen, to apply LLVM operand constraints.
+    #
+    # LLVM does not appear(?) to be able to model a single operand which is both an input and an
+    # output. For example, a two operand add:
+    #
+    #   add $acc, $val
+    #
+    # Maybe be in fact modelled as:
+    #
+    #   asm: "add $acc, $val"
+    #   inputs: $acc, $val
+    #   outputs: $dest
+    #   constraint: $dest = $acc
+    #
+    # This is unintuitive if you don't care about LLVM's semantics - you'll see `$dest` in the list
+    # of operands, despite the fact it doesn't appear in the Assembly string.
+    #
+    # This method will retroactively fix up the input and output operands, so that you're only left
+    # with the ones that appear in the Assembly string:
+    #
+    #   asm: "add $acc, $val"
+    #   inputs: $acc, $val
+    #   outputs: $acc
+    # 
+    # This breaks the direct relationship with the LLVM TableGen data, but improves usability, so
+    # it's worth doing.
+    private def apply_constraints(constraints)
+      return if constraints.strip.empty?
+
+      # We only currently support the constraint "$x = $y"
+      unless /^\s*\$([A-Za-z0-9]+)\s*=\s*\$([A-Za-z0-9]+)\s*$/ === constraints
+        puts "WARNING: Dropping unknown constraint applied to `#{name}`: `#{constraints}`"
+        return
+      end
+
+      left = $1
+      right = $2
+
+      # Figure out which of the two sides of the constraint actually appears in the Assembly string
+      operands_in_asm = assembly_format
+        .scan(/\$[A-Za-z0-9]+/)
+        .map { |op| op[1..-1] } # Drop leading $
+
+      if operands_in_asm.include?(left) && operands_in_asm.include?(right)
+        # Ah, they're both mentioned! why is there a constraint then?
+        # Whatever. Leave things as they are
+        return
+      elsif operands_in_asm.include?(left)
+        rename_operand(old_name: right, new_name: left)
+      elsif operands_in_asm.include?(right)
+        rename_operand(old_name: left, new_name: right)
+      else
+        # Neither operand is in the assembly! huh?
+        puts "WARNING: Operands in `#{name}` involved in constraint do not exist in Assembly string: `#{constraints}`"
+      end
+    end
+
+    # Find occurrences of an input/output operand and rename it.
+    private def rename_operand(old_name:, new_name:)
+      @inputs.each  { |op| op.name = new_name if op.name == old_name }
+      @outputs.each { |op| op.name = new_name if op.name == old_name }
     end
   end
 end
