0300d1f42bc26a18d562e052934de7582bc1396e
Aaron Christiansen <aaronc20000@gmail.com>
2024-11-25 22:29:02 +0000

    Don't link to dead pages of x86 docs

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'sass-embedded'
+gem 'nokogiri'
+gem 'http'
--- lib/documentation/felix_cloutier_x86_provider.rb
+++ lib/documentation/felix_cloutier_x86_provider.rb
@@ -1,25 +1,57 @@
 require_relative 'provider'
 
+require 'http'
+require 'nokogiri'
+
 module Documentation
   # A documentation provider which links to Félix Cloutier's unofficial x86 documentation.
   class FelixCloutierX86Provider < Provider
-    def documentation_url(ins)
-      # TODO: test existence, could scrape table on index first
-      
+    ENDPOINT = 'https://www.felixcloutier.com/x86'
+
+    def initialize
+      @mnemonic_links = scrape_documentation_links
+    end
+
+    def documentation_url(ins)      
       # Pages are always named after the Intel opcode
       intel_opcode = ins
         .assembly_format_for_variant(1)
         .split
         .first
       
-      if intel_opcode
-        Link.new(
-          "https://www.felixcloutier.com/x86/#{intel_opcode.downcase}",
-          "Documentation (Félix Cloutier)"
-        )
-      else
-        nil
+      @mnemonic_links[intel_opcode]
+    end
+
+    private def scrape_documentation_links
+      links = {}
+
+      raw_content = HTTP.get(ENDPOINT).body.to_s
+      xml_content = Nokogiri::XML(raw_content)
+
+      xml_content.css("tr").each do |row|
+        # The page URLs are formatted like:
+        #   /x86/fld1:fldl2t:fldl2e:fldpi:fldlg2:fldln2:fldz
+        # for all of the different Intel mnemonics they cover
+        link = row.css("td a")&.first
+        next unless link
+        href = link.attributes['href']
+        next unless /^\/x86\/(.+)$/ === href
+        page_name = $1
+        mnemonics = page_name.split(':')
+
+        # Get instruction description
+        desc = row.css("td").last.text
+
+        # Add link for each mnemonic
+        mnemonics.each do |mnemonic|
+          links[mnemonic] = Link.new(
+            "#{ENDPOINT}/#{page_name}",
+            "\"#{desc}\" (Félix Cloutier)"
+          )
+        end
       end
+
+      links
     end
   end
 end
