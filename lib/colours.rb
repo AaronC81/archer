3438a0f02a1f1f11c28526afc86f081b20a6680b
Aaron Christiansen <aaronc20000@gmail.com>
2024-12-24 14:06:27 +0000

    Remove duplicate colour entry

--- lib/colours.rb
+++ lib/colours.rb
@@ -16,7 +16,6 @@ module Colours
     "Address register" => Pair.new("fca85d", "35342f"), # light orange - dark grey
     "Data register" => Pair.new("fe9f97", "35342f"), # light pink - dark grey
     "General-purpose register" => Pair.new("08327d", "fbfbfb"), # dark blue - white
-    "General-purpose register" => Pair.new("08327d", "fbfbfb"), # dark blue - white
 
     "Other register" => Pair.new("db2f27", "fbfbfb"), # red - white
 
