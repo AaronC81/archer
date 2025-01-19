6e34a5e27a1f34e191c3bbe17ce955c63b71aeff
Aaron Christiansen <aaronc20000@gmail.com>
2025-01-19 19:36:37 +0000

    Fix tests after TriCore data change

--- spec/instruction_link_spec.rb
+++ spec/instruction_link_spec.rb
@@ -9,21 +9,21 @@ feature 'Instruction linking' do
     input_operand_filter("Data register").set(true)
     output_operand_filter("Address register").set(true)
 
-    expect(page).to have_link('MOV_Arr', href: '#MOV_Arr')
+    expect(page).to have_link('MOV_A_rr', href: '#MOV_A_rr')
 
     # Navigate there
-    click_on 'MOV_Arr'
+    click_on 'MOV_A_rr'
 
     # Check page has updated
-    expect(page.current_url).to end_with('#MOV_Arr')
+    expect(page.current_url).to end_with('#MOV_A_rr')
     expect(page).to have_content 'mov.a'
     expect(page).to have_content 'Currently showing one specific instruction'
   end
 
   it 'works when the page is visited directly' do
-    visit '/target/TriCore#MOV_Arr'
+    visit '/target/TriCore#MOV_A_rr'
     
-    expect(page.current_url).to end_with('#MOV_Arr')
+    expect(page.current_url).to end_with('#MOV_A_rr')
     expect(page).to have_content 'mov.a'
     expect(page).to have_content 'Currently showing one specific instruction'
   end
