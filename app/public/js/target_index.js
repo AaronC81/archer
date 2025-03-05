781a02d4010d0a22ad5b8a346a5f519a93b05c58
Aaron Christiansen <aaronc20000@gmail.com>
2025-03-05 22:19:40 +0000

    Better rendering of no inputs or outputs

--- app/public/js/target_index.js
+++ app/public/js/target_index.js
@@ -8,6 +8,18 @@ class Instruction {
     constructor(obj) {
         Object.assign(this, obj)
     }
+    
+    hasAnyInputs() {
+        return this.inputs.length > 0 ||
+               this.implicitInputs.length > 0 ||
+               this.mayLoad;
+    }
+
+    hasAnyOutputs() {
+        return this.outputs.length > 0 ||
+               this.implicitOutputs.length > 0 ||
+               this.mayStore;
+    }
 }
 
 class DataManager {
@@ -176,45 +188,67 @@ function refreshFilters() {
                     <a href="#${instruction.name}">${instruction.name}</a>
                 </div>
                 <table class="result-operand-table">
-                    <tr>
-                        <th colspan="2">Inputs</th>
-                    <tr>
-                    ${
-                        instruction.inputs
-                            .map(i => `
-                                <tr>
-                                    <td class="label-cell"><mark style="${i.operandTypeFamilyStyle}"><code>${i.name}</code></mark></td>
-                                    <td>${i.operandType}</td>
-                                </tr>`
-                            )
-                            .join("")
-                    }
-                    ${
-                        instruction.implicitInputs
-                            .map(i => `<tr><td class="label-cell">Implicit</td><td><code>${i}</code></td></tr>`)
-                            .join("")
-                    }
-                    ${ instruction.mayLoad ? "<tr><td></td><td>Loads memory</td></tr>" : "" }
 
-                    <tr>
-                        <th colspan="2">Outputs</th>
-                    <tr>
                     ${
-                        instruction.outputs
-                            .map(i => `
-                                <tr>
-                                    <td class="label-cell"><mark style="${i.operandTypeFamilyStyle}"><code>${i.name}</code></mark></td>
-                                    <td>${i.operandType}</td>
-                                </tr>`
-                            )
-                            .join("")
+                        instruction.hasAnyInputs()
+                        ? `
+                            <tr>
+                                <th colspan="2">Inputs</th>
+                            <tr>
+                            ${
+                                instruction.inputs
+                                    .map(i => `
+                                        <tr>
+                                            <td class="label-cell"><mark style="${i.operandTypeFamilyStyle}"><code>${i.name}</code></mark></td>
+                                            <td>${i.operandType}</td>
+                                        </tr>`
+                                    )
+                                    .join("")
+                            }
+                            ${
+                                instruction.implicitInputs
+                                    .map(i => `<tr><td class="label-cell">Implicit</td><td><code>${i}</code></td></tr>`)
+                                    .join("")
+                            }
+                            ${ instruction.mayLoad ? "<tr><td></td><td>Loads memory</td></tr>" : "" }
+                        `
+                        : `
+                            <tr class="quiet">
+                                <th class="quiet" colspan="2">No Inputs</th>
+                            </tr>
+                        `
                     }
+                    
                     ${
-                        instruction.implicitOutputs
-                            .map(i => `<tr><td class="label-cell">Implicit</td><td><code>${i}</code></td></tr>`)
-                            .join("")
+                        instruction.hasAnyOutputs()
+                        ? `
+                            <tr>
+                                <th colspan="2">Outputs</th>
+                            <tr>
+                            ${
+                                instruction.outputs
+                                    .map(i => `
+                                        <tr>
+                                            <td class="label-cell"><mark style="${i.operandTypeFamilyStyle}"><code>${i.name}</code></mark></td>
+                                            <td>${i.operandType}</td>
+                                        </tr>`
+                                    )
+                                    .join("")
+                            }
+                            ${
+                                instruction.implicitOutputs
+                                    .map(i => `<tr><td class="label-cell">Implicit</td><td><code>${i}</code></td></tr>`)
+                                    .join("")
+                            }
+                            ${ instruction.mayStore ? "<tr><td></td><td>Stores memory</td></tr>" : "" }
+                        `
+                        : `
+                            <tr class="quiet">
+                                <th class="quiet" colspan="2">No Outputs</th>
+                            </tr>
+                        `
                     }
-                    ${ instruction.mayStore ? "<tr><td></td><td>Stores memory</td></tr>" : "" }
+                    
                 </table>
                 <div class="predicate-labels">
                     ${
