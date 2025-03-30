e35b3714fb3e2872f67febf0851f5b160786e6ad
Aaron Christiansen <aaronc20000@gmail.com>
2025-03-30 22:50:50 +0100

    Add basic predicate test

---
+++ spec/predicate_filter_spec.rb
@@ -0,0 +1,25 @@
+require 'capybara/dsl'
+require_relative 'ui/filters'
+
+feature "Predicate filtering" do
+  it "shows and hides based on selected predicates" do
+    visit '/target/X86'
+
+    # This is an "x87 FPU" instruction
+    # All predicates are selected by default, so this should show
+    mnemonic_filter.set 'fabs'
+    expect(page).to have_content 'ABS_F' # LLVM name
+
+    # Untick that filter - it goes away
+    predicate_filter('x87 FPU').set(false)
+    expect(page).not_to have_content 'ABS_F'
+
+    # Tick the filter - comes back
+    predicate_filter('x87 FPU').set(true)
+    expect(page).to have_content 'ABS_F'
+
+    # Untick an irrelevant filter - still there
+    predicate_filter('3DNow!').set(false)
+    expect(page).to have_content 'ABS_F'
+  end
+end
--- spec/ui/filters.rb
+++ spec/ui/filters.rb
@@ -12,3 +12,11 @@ def no_output_operands_filter = find(id: 'input-operand-output-none-filter')
   
 def memory_store_filter = find(id: 'input-store-filter')
 def memory_load_filter = find(id: 'input-load-filter')
+
+def mnemonic_filter = find(id: 'input-mnemonic-filter')
+
+def predicate_filter(name)
+  find("#predicate-filter-table")
+    .find("tr", text: name)
+    .find("td input")
+end
