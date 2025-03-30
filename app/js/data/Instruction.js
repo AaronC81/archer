53769fe49be938758b9fac2485b0a449089cee22
Aaron Christiansen <aaronc20000@gmail.com>
2025-03-30 16:39:06 +0100

    Break out instruction filter logic

--- app/js/data/Instruction.js
+++ app/js/data/Instruction.js
@@ -8,6 +8,60 @@ export default class Instruction {
     constructor(obj) {
         Object.assign(this, obj)
     }
+
+    matchesFilters(filters, anchor, assemblyVariant) {
+        const {
+            mnemonic,
+            memoryStore, memoryLoad,
+            inputNone, inputFamilies,
+            outputNone, outputFamilies,
+            predicateNone, predicates,
+        } = filters;
+
+        // Specific instruction filter
+        if (anchor && this.name != anchor)
+            return false;
+
+        // Memory characteristic filters
+        if (memoryStore && !this.mayStore)
+            return false;
+        if (memoryLoad && !this.mayLoad)
+            return false;
+
+        // Textual filter
+        if (mnemonic && !this.assemblyVariants[assemblyVariant].mnemonic.includes(mnemonic))
+            return false;
+
+        // Operand filters
+        if (inputFamilies.size > 0) {
+            if (![...inputFamilies].every(o => this.inputs.map(i => i.operandTypeFamily).includes(o)))
+                return false;
+        }
+        if (outputFamilies.size > 0) {
+            if (![...outputFamilies].every(o => this.outputs.map(i => i.operandTypeFamily).includes(o)))
+                return false;
+        }
+        if (inputNone && this.inputs.length > 0)
+            return false;
+        if (outputNone && this.outputs.length > 0)
+            return false;
+
+        // Predicate filters
+        // Either one of the following must be true for the instruction to show up:
+        //   (1) This instruction has no predicates, and the user has ticked "None", and 
+        //   (2) This instruction has some predicates, and the user has ticked all of them
+        if (this.predicates.length == 0) {
+            // (1)
+            if (!predicateNone)
+                return false;
+        } else {
+            // (2)
+            if (!this.predicates.every(p => predicates.has(p.friendly_name)))
+                return false;
+        }
+
+        return true;
+    }
     
     hasAnyInputs() {
         return this.inputs.length > 0 ||
