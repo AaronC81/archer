3ee840be3eceb2c3535cf3da1331d1bddccd2c51
Aaron Christiansen <aaronc20000@gmail.com>
2025-03-30 17:41:19 +0100

    Fix anchor behaviour using new `useAnchor` hook

---
+++ app/js/hook/useAnchor.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+/**
+ * Hook to relate component state to the current browser anchor (or 'hash') - the bit after the
+ * `#` in the URL.
+ * 
+ * Returns tuple of:
+ *   - The current anchor as of rendering (without the leading `#` character)
+ *   - Function to change the anchor - pass a string (without the leading `#` character)
+ * 
+ * A component which uses this hook is re-rendered if the URL hash changes.
+ */
+export default function useAnchor() {
+    const [anchor, internalSetAnchor] = useState(() => window.location.hash.substring(1));
+
+    // Trigger render if anchor changes while the page is open
+    window.onhashchange = () => {
+        internalSetAnchor(window.location.hash.substring(1));
+    };
+
+    // If the user calls `setAnchor`, update the page too
+    const setAnchor = (anchor) => {
+        window.location.hash = anchor;
+        internalSetAnchor(anchor);
+    }
+
+    return [anchor, setAnchor];
+}
